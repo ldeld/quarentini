26db1512a53426f9a5be5825b291373c43f6e91b
Lorenzo Del Castillo Detoeuf <l.delcastillo.97@gmail.com>
2020-04-09 19:19:45 +0200

    bs setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'rooms/create'
+  get 'rooms/show'
+  get 'pages/home'
+
+  root to: "pages#home"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
