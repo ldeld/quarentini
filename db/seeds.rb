cff2da358eb4e02c82ad6419e41b94078b962b84
Lorenzo Del Castillo Detoeuf <l.delcastillo.97@gmail.com>
2020-04-10 21:14:46 +0200

    basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+
+card_data = JSON.parse(File.read(Rails.root + "db/cards.json"))
+
+puts "creating cards..."
+
+if Rails.env.development?
+  10.times do |i|
+    Card.create!(name: "test card #{i}", content: "This is test card #{i}. I should only exist in development!")
+  end
+end
+
+puts "done!"
