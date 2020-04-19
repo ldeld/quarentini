6624aad3c5329f2a354f87aa0c0c2bd07a976361
Lorenzo Del Castillo Detoeuf <l.delcastillo.97@gmail.com>
2020-04-20 00:28:50 +0200

    moved channel js to stimulus controller

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,34 +1,35 @@
 import consumer from "./consumer"
 
 // TODO: place this in a stimulus controller?
-const roomContainer = document.getElementById('room-container');
-if (roomContainer) {
-  consumer.subscriptions.create({
-      channel: "RoomChannel",
-      slug: roomContainer.dataset.roomSlug
-    },
-    {
-      connected() {
-          // Called when the subscription is ready for use on the server
-      },
+// const roomContainer = document.getElementById('room-container');
+// if (roomContainer) {
+//   consumer.subscriptions.create({
+//       channel: "RoomChannel",
+//       slug: roomContainer.dataset.roomSlug
+//     },
+//     {
+//       connected() {
+//           // Called when the subscription is ready for use on the server
+//       },
 
-      disconnected() {
-        // Called when the subscription has been terminated by the server
-      },
+//       disconnected() {
+//         // Called when the subscription has been terminated by the server
+//       },
 
-      received(data) {
-        console.log(data)
-        switch (data.action) {
-          case 'playerJoin':
-            const playerDiv = document.querySelector(".players");
-            const html = `<li>${data.body}</li>`;
-            playerDiv.insertAdjacentHTML("beforeend", html);
-          case 'gameStart':
-            window.location.reload();
-          case 'cardNext':
-            const cardDiv = document.querySelector("#current-card");
-            cardDiv.innerHTML = data.body;
-        }
-      }
-  });
-}
+//       received(data) {
+//         console.log(data)
+//         switch (data.action) {
+//           case 'playerJoin':
+//             const playerDiv = document.querySelector(".lobby-players");
+//             const html = `<li>${data.body}</li>`;
+//             playerDiv.insertAdjacentHTML("beforeend", html);
+//           case 'gameStart':
+//             window.location.reload();
+//           case 'cardNext':
+//             // const cardDiv = document.querySelector("#current-card");
+//             // cardDiv.innerHTML = data.card_body;
+//             // console.log(data.active_player_id);
+//         }
+//       }
+//   });
+// }
---
+++ app/javascript/controllers/game_controller.js
@@ -0,0 +1,34 @@
+import { Controller } from "stimulus"
+import consumer from '../channels/consumer';
+
+export default class extends Controller {
+  static targets = ["card"]
+
+  connect() {
+    console.log(this.data.get("slug"))
+    this.channel = consumer.subscriptions.create({
+      channel: "RoomChannel",
+      slug: this.data.get("slug")
+    },
+    {
+      connected: this._cableConnected.bind(this),
+      disconnected: this._cableDisconnected.bind(this),
+      received: this._cableReceived.bind(this)
+    });
+  }
+
+  _cableConnected() {
+    // Called when the subscription is ready for use on the server
+  }
+
+  _cableDisconnected() {
+    // Called when the subscription has been terminated by the server
+  }
+
+  _cableReceived(data) {
+    // Called when there's incoming data on the websocket for this channel
+    console.log(this)
+    this.cardTarget.innerHTML = data.card_body;
+    console.log(data.active_player_id);
+  }
+}
